3ffab92e6e65f164e0ee53d37a4b95dc5e58c617
TohWeiJie01 <tohweijie01@gmail.com>
2021-10-24 19:50:57 +0800

    Add Notes class

---
+++ src/main/java/seedu/address/model/notes/Notes.java
@@ -0,0 +1,52 @@
+package seedu.address.model.notes;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+/**
+ * Represents the notes about an applicant in the address book.
+ */
+public class Notes {
+
+    public static final String MESSAGE_CONSTRAINTS = "Notes should not be blank";
+
+    public final String information;
+
+    /**
+     * Constructs a {@code Notes}.
+     *
+     * @param information A valid level of education.
+     */
+    public Notes(String information) {
+        requireNonNull(information);
+        checkArgument(isValidNotes(information), MESSAGE_CONSTRAINTS);
+        this.information = information;
+    }
+
+    /**
+     * Returns true if a given string is valid information for Notes.
+     *
+     * @param test String to be tested.
+     * @return Boolean indicating if given string is valid information for Notes.
+     */
+    public static boolean isValidNotes(String test) {
+        return !test.isBlank();
+    }
+
+    @Override
+    public String toString() {
+        return information;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Notes // instanceof handles nulls
+                && information.equals(((Notes) other).information)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return information.hashCode();
+    }
+}
